6a2ca26eef5a8184c7f20bb905f70c229f41abf7
andrewipstirling <andrewipstirling@gmail.com>
2024-03-28 14:37:55 -0400

    added pdf

--- kalman.py
+++ kalman.py
@@ -364,13 +364,12 @@ $$
 """
 # %%
 class KalmanFilter():
-    def __init__(self,A:np.ndarray,B:np.ndarray,C:np.ndarray,
-                 Q:np.ndarray,R:np.ndarray,x0:np.ndarray, P0:np.ndarray):
+    def __init__(self,A:np.ndarray,B:np.ndarray,C:np.ndarray,Q:np.ndarray,R:np.ndarray,x0:np.ndarray, P0:np.ndarray):
         self.A = A
         self.B = B
         self.C = C
-        self.Q = Q
-        self.R = R
+        self.Q = Q  # Covariance
+        self.R = R  # Covariance
         self.P_k = P0
         self.K_k = None
         # State
@@ -378,19 +377,18 @@ class KalmanFilter():
         # Measurements
         self.u_acc = None
         self.y_k = None
-
     def get_measurement(self, u_acc, y_k):
         self.u_acc = np.array([[u_acc]])
         self.y_k = np.array([[y_k]])
 
     def predict(self) -> np.ndarray:
-        # Insert Prediction step
+        # Predict Using Constant Velocity Dynamics
         self.x = self.A @ self.x + self.B @ self.u_acc
         self.P_k = self.A @ self.P_k @ self.A.T + self.Q
         return self.x
     
     def correct(self) -> None:
-        # Insert Correction Step
+        # Correct using measurement, covariance
         tmp = (self.C @ self.P_k @ self.C.T) + self.R
         self.K_k = self.P_k @ self.C.T @ np.linalg.inv(tmp)
         self.x = self.x + self.K_k @ (self.y_k - (self.C @ self.x))
